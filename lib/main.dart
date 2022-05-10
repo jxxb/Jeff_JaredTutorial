c9644dac9de7b624f65afde8c50ba7c3495fb3ff
Jared Barney <jared.barney97@gmail.com>
2022-05-10 15:02:36 -0600

    Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(App());
-}
+// void main() {
+//   runApp(App());
+// }
+
+void main() => runApp(App());
+
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hey Bro.'),);
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('Hi Jeff :)')),
+      body: Text('My dude'),
+    ),);
   }
 }
