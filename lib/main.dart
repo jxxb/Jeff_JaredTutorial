44e8baa3b06b0ba3ac1440cd9034e9b57357ecaa
Jared Barney <jared.barney97@gmail.com>
2022-05-10 14:29:16 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hey Bro.'),);
+  }
+}
