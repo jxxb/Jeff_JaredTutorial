ec9b894bb78946e3225697dcca71e435f7eef826
Jared Barney <jared.barney97@gmail.com>
2022-05-19 15:26:46 -0600

    Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './question.dart';
 // void main() {
 //   runApp(App());
 // }
@@ -7,12 +7,58 @@ import 'package:flutter/material.dart';
 void main() => runApp(App());
 
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    // throw UnimplementedError();
+    return _AppState();
+  }
+}
+class _AppState extends State<App> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex++;
+      print(_questionIndex);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Hi Jeff :)')),
-      body: Text('My dude'),
-    ),);
+    var questions = [
+      'What\'s your favorite genre Jeff?',
+      'What\'s your favorite song Jeff?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hi Jeff :)'
+          ),
+       ),
+      body: Column(children: [
+        Question(
+        questions[_questionIndex]
+        ),
+        ElevatedButton(
+          child: Text('Answer 1'),
+          onPressed: answerQuestion,
+        ),
+        ElevatedButton(
+          child: Text('Answer 2'),
+          onPressed: () => print('Answer 2 chosen!'),
+        ),
+        ElevatedButton(
+          child: Text('Answer 3'),
+          onPressed: () {
+            print('Pick number 3 mahLord');
+        },
+        ),
+      ],
+      ),
+    ),
+    );
   }
 }
---
+++ lib/question.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(width: double.infinity ,child: Text(
+
+      questionText,
+      style: TextStyle(fontSize: 28),
+      textAlign: TextAlign.center,
+    ),);
+  }
+}
